432bd4a9636f15942cfd3a652505f1b78574b3fa
IT21185298 <it21185298@my.sliit.lk>
2024-01-15 23:43:43 +0530

    BottomNavigationBar is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/pages/navpages/main_page.dart';
 import 'package:travel_app/pages/welcome_page.dart';
 
 import 'package:device_preview/device_preview.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       
-      home: const Welcomepage(),
+      home: const MainPage(),
     );
   }
 }
